462fa088fb0c3a02c176711288172be528067d99
nsanders <nsanders@paypal.com>
2016-04-21 17:08:04 -0700

    Updating Styles and CSS

--- AngularAppInContext/client/app/scripts/api.service.js
+++ AngularAppInContext/client/app/scripts/api.service.js
@@ -20,6 +20,7 @@ angular.module('clientApp').service('apiModel', function($http, $cookies, $locat
 	}
 
 	function getPaymentDetails() {
+		$('#loading').show();
 		var reqUrl = '/api/getPaymentDetails';
         var config = {
             'xsrfHeaderName': 'X-CSRF-TOKEN',
@@ -28,6 +29,10 @@ angular.module('clientApp').service('apiModel', function($http, $cookies, $locat
         return $http.post(reqUrl, model.queryParams, config).
 	        success(function (response) {
 	            model.paymentDetails = response;
+	            $timeout(function() {
+                	$('#loading').hide();
+	            	$('#paymentDetails').show();
+            	}, 2000);
 	            console.log(model.paymentDetails);
 	        }).
 	        error(function (response) {
@@ -37,6 +42,8 @@ angular.module('clientApp').service('apiModel', function($http, $cookies, $locat
 	}
 
 	function executePayment() {
+		$('#paymentDetails').hide();
+		$('#loading').show();
 		var reqUrl = '/api/executePayment';
         var config = {
             'xsrfHeaderName': 'X-CSRF-TOKEN',
@@ -45,7 +52,10 @@ angular.module('clientApp').service('apiModel', function($http, $cookies, $locat
         return $http.post(reqUrl, model.paymentDetails, config).
 	        success(function (response) {
 	            model.completePaymentDetails = response;
-	            model.executePaymentButton = false;
+	            $timeout(function() {
+                	$('#loading').hide();
+	            	$('#completePaymentDetails').show();
+            	}, 2000);
 	            console.log(model.completePaymentDetails);
 	        }).
 	        error(function (response) {
@@ -56,22 +66,80 @@ angular.module('clientApp').service('apiModel', function($http, $cookies, $locat
 
 	var model = {
 		pmtData: {
-	        intent:'sale',
-	        redirect_urls:{
-	            return_url:'http://localhost:3000/#/return',
-	            cancel_url:'http://localhost:3000/#/cancel'
-	        },
-	        payer:{
-	            payment_method:'paypal'
-	        },
-	        transactions:[{
-	            amount:{
-	                total:'7.47',
-	                currency:'USD'
-	            },
-	            description:'This is the payment transaction description.'
-	        }]
-	    },
+			"intent":"sale",
+			"redirect_urls":
+				{
+					"return_url":"http://localhost:3000/#/return",
+					"cancel_url":"http://localhost:3000/#/cancel"
+				},
+			"payer":
+				{
+					"payment_method":"paypal"
+				},
+			"transactions":[
+				{
+					"amount":
+					{
+						"total":"15.00",
+						"currency":"USD",
+						"details":
+							{
+								"shipping":"2.00",
+								"subtotal":"13.00",
+								"tax":"0.00",
+								"handling_fee":"0.00",
+								"insurance":"0.00",
+								"shipping_discount":"0.00"
+							}
+					},
+					"description":"My Payment Description",
+					"invoice_number": "ABCDEFGHIJK" + Date.now(),
+					"item_list":
+					{
+						"items":
+						[
+							{
+								"quantity":"1",
+								"name":"Item 0 Name",
+								"price":"7.00",
+								"currency":"USD",
+								"sku":"ABCDEFGHIJKL987654321",
+								"description":"Item 0 Description",
+								"tax":"0.00"
+							},
+							{
+								"quantity":"1",
+								"name":"Item 1 Name",
+								"price":"2.00",
+								"currency":"USD",
+								"sku":"ABCDEFGHIJKL987654322",
+								"description":"Item 1 Description",
+								"tax":"0.00"
+							},
+							{
+								"quantity":"1",
+								"name":"Item 2 Name",
+								"price":"2.00",
+								"currency":"USD",
+								"sku":"ABCDEFGHIJKL987654323",
+								"description":"Item 2 Description",
+								"tax":"0.00"
+							},
+							{
+								"quantity":"1",
+								"name":"Item 3 Name",
+								"price":"2.00",
+								"currency":"USD",
+								"sku":"ABCDEFGHIJKL987654324",
+								"description":"Item 3 Description",
+								"tax":"0.00"
+							}
+						]
+					}
+				}
+			]
+		},
+		currencySymbol: '$ ',
 	    createPaymentResponse: {},
 	    queryParams: {},
 	    paymentDetails: {},
